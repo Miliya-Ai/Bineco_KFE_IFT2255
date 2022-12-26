00189f440c5227b0e76530cfed2a2b901c889684
Flo_JB <eedfloryjb06@gmail.com>
2022-12-25 20:02:37 -0500

    code changement LOGin

--- src/main/java/FindConsommateur.java
+++ src/main/java/FindConsommateur.java
@@ -79,7 +79,7 @@ public class FindConsommateur extends Page{
         String[][] formattedCons = new String[10][8];
 
         for(int k = 0; k < lots.length; k++){
-            System.out.println("Code : "+lots[k][0]+"  Nom : "+lots[k][1]);
+            System.out.println("Code : "+lots[k][0]+"  Nom : "+lots[k][2]);
         }
 
         out("--Appuyez sur 0 pour revenir au menu--");
