2b74e11d8a8df8eba3ee49de03150c761c480874
Flo_JB <eedfloryjb06@gmail.com>
2022-12-25 23:32:16 -0500

    code final

--- src/test/java/MunicipInfoTest.java
+++ src/test/java/MunicipInfoTest.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class MunicipInfoTest {
+    /**
+     * @author Miliya Ai
+     * @throws IOException
+     */
 
     @Test
     void toInt() throws IOException {
@@ -17,5 +22,31 @@ class MunicipInfoTest {
        assertEquals(test.toInt(testNumero)[0][0],attendu[0][0]);
 
     }
+    /**
+     * S'assurer que le fichier cherche la bonne information
+     * @author Flory
+     * @throws IOException
+     */
+    @Test
+    void toArrL() throws IOException {
+        UserController testController = new UserController();
+        ArrayList test = new ArrayList();
+        test.add("test1 test helloo");
+        assertEquals(testController.toArrL("src/test/java/toArrLTest.txt").get(0), test.get(0));
+    }
 
+    /**
+     * Verifie que les informations d'une liste se transforme bien en un array de String.
+     * @author Flory
+     * @throws IOException La methode utilise des methodes qui lisent des fichiers.
+     *                     Si le fichier n'est pas present, une exception se produit.
+     */
+    @Test
+    void getFromCsv() throws IOException {
+        MunicipInfo test = new MunicipInfo();
+        String[] data = new String[1];
+        data [0] = "nom";
+        String [][] formatedTest = new String[][]{data};
+        assertArrayEquals(test.getFromCsv("src/test/java/getFromCSVTest.txt"), formatedTest);
+    }
 }
