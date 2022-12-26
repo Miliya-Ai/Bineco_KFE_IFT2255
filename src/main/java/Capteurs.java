83b2f9697b0079a44790ebf6b7ec3c458a28c4d9
Flo_JB <eedfloryjb06@gmail.com>
2022-12-25 20:56:02 -0500

    code changement resident

--- src/main/java/Capteurs.java
+++ src/main/java/Capteurs.java
@@ -32,16 +32,16 @@ public class Capteurs {
      */
     public void setCapteurs(){
 
-        capteursList.put("r00", new String[] {"0,70", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
-        capteursList.put("co00", new String[] {"0,55", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
+        capteursList.put("r00", new String[] {"0,70", "plastique : 0,50;verres: 0,25;metaux : 0,25"});
+        capteursList.put("co00", new String[] {"0,55", "residus de jardin : 0,50;residus de table : 0,25;litiere d'animaux : 0,25"});
         capteursList.put("d00", new String[] {"0,82"});
 
-        capteursList.put("r01", new String[] {"0,33", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
-        capteursList.put("co01", new String[] {"0,75", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
+        capteursList.put("r01", new String[] {"0,33", "papiers : 0,50;cartons : 0,25;metaux : 0,25"});
+        capteursList.put("co01", new String[] {"0,75", "pelures : 0,50;produits laitiers: 0,25;feuilles mortes: 0,25"});
         capteursList.put("d01", new String[] {"0,50"});
 
-        capteursList.put("r02", new String[] {"0,25", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
-        capteursList.put("co02", new String[] {"0,80", "categorie1 : 0,50;categorie2 : 0,25;categorie3 : 0,25"});
+        capteursList.put("r02", new String[] {"0,25", "plastique : 0,50;cartons : 0,25;papiers: 0,25"});
+        capteursList.put("co02", new String[] {"0,80", "litiere d'animaux : 0,50;pelures : 0,25;feuilles mortes : 0,25"});
         capteursList.put("d02", new String[] {"0,65"});
 
     }
